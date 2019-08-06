dcd3f39a7d9f3ab3e71612230bf8f702a0e83b8e
sz <shazina@usec.io>
2019-08-06 18:06:33 +0800

    chore: install specified certificate file

--- certs/install_ca_cert.sh
+++ certs/install_ca_cert.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# Installs certauth.crt to the trusted certs folder for the distribution
+# Bash is needed for $RANDOM to work
+# Installs specified cert file to the trusted certs folder for the distribution
 # (CentOS or Debian/Ubuntu)
 #
 #
@@ -7,17 +8,21 @@
 #     ./install_ca_cert.sh [options]
 # Options:
 #     -o <operating system>    # Operating system "debian" or "centos"
-# certauth.crt  must be in the same directory as this script
+#     -c <certificate file>
+
 
 if [ $# -eq 0 ]; then
     echo "No command specified" >&2
     exit 1
 fi
 
-while getopts ":o:" opt; do
+while getopts ":o:c:" opt; do
   case $opt in
     o)
       OS=$OPTARG
+      ;;
+    c)
+      CERTIFICATE=$OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -35,17 +40,21 @@ if [ "$OS" != "debian" ] && [ "$OS" != "centos" ]; then
     exit 1
 fi
 
+if [ ! -f $CERTIFICATE ]; then
+  echo "Certificate $CERTIFICATE does not exist." >&2
+  exit 1
+fi
+
 # Install the CA cert in the appropriate directory
-if [ $OS = "debian" ]
-  then
+if [ $OS = "debian" ];  then
     if [ $EUID -ne 0 ]
       then
-        sudo cp certauth.crt /usr/local/share/ca-certificates/$RANDOM.crt
+        sudo cp $CERTIFICATE /usr/local/share/ca-certificates/$RANDOM.crt
         # update ca certificates
         # This adds appends the cert to /etc/ssl/certs/ca-certificates.crt
         sudo update-ca-certificates --fresh
       else
-        cp certauth.crt /usr/local/share/ca-certificates/$RANDOM.crt
+        cp $CERTIFICATE /usr/local/share/ca-certificates/$RANDOM.crt
         update-ca-certificates --fresh
     fi
 fi
@@ -54,11 +63,11 @@ if [ $OS = "centos" ]; then
   if [ $EUID -ne 0 ]; then
     sudo update-ca-trust force-enable
     # update ca certificates
-    sudo cp certauth.crt /etc/pki/ca-trust/source/anchors/$RANDOM.crt
+    sudo cp $CERTIFICATE /etc/pki/ca-trust/source/anchors/$RANDOM.crt
     sudo update-ca-trust extract
   else
     update-ca-trust force-enable
-    cp certauth.crt /etc/pki/ca-trust/source/anchors/$RANDOM.crt
+    cp $CERTIFICATE /etc/pki/ca-trust/source/anchors/$RANDOM.crt
     update-ca-trust extract
   fi
 fi
